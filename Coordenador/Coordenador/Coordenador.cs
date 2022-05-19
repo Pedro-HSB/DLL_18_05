eb80634c889cd14adc1a5a33cbbf829f3c7cfb0d
Pedro <Pedro.bernardo8@etec.sp.gov.br>
2022-05-18 22:29:15 -0300

    s+6954l3.a

---
+++ Coordenador/Coordenador/Coordenador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pessoa;
+
+namespace Coordenador
+{
+    public class Coordenador : Pessoa.Pessoa
+    {
+        public string equipe;
+    }
+}
