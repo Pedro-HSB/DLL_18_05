575d28f7f5ec10c15ef108a98ba900a82b3a178e
pedro <pedro.bernardo8@etec.sp.gov.br>
2022-05-22 20:10:02 -0300

    finished_DLL

---
+++ DLL_25-05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dll_P;
+using Dll_Clie;
+using Dll_Coor;
+using Dll_Di;
+using Dll_Funci;
+using Dll_Ge;
+using Dll_Super;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Pessoa Main = new Pessoa();
+        // Main.MostarNome();
+            string nome;
+    }
+}
